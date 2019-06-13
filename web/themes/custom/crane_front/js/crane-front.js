3c4ecb90aedc8cf3e6b823727050bc34e40a93d7
Adam Fuchs <adam@electriccitizen.com>
2019-06-13 14:40:31 -0500

    Quicklinks menu theming

--- web/themes/custom/crane_front/js/crane-front.js
+++ web/themes/custom/crane_front/js/crane-front.js
@@ -11,10 +11,10 @@ Drupal.behaviors.pageMenu = {
 			$(document).on('click','#block-quicklinks .item-level-1 > a',function(e){
 				e.preventDefault();
         if($(this).attr('aria-expanded') == 'true'){
-          $(this).attr('aria-expanded', "false").siblings('ul').slideUp(300).attr('aria-hidden', 'true').end().closest('li').removeClass('expanded');
+          $(this).attr('aria-expanded', "false").siblings('ul').animate({'left':'-310'}, 300).attr('aria-hidden', 'true').end().closest('li').removeClass('expanded');
         }else{
-        	$('#block-quicklinks li.expanded').removeClass('expanded').find('> a').attr('aria-expanded', 'false').siblings('ul').slideUp(300).attr('aria-hidden', 'true');
-          $(this).attr('aria-expanded', "true").siblings('ul').slideDown(300).attr('aria-hidden', 'false').end().closest('li').addClass('expanded');
+        	$('#block-quicklinks li.expanded').removeClass('expanded').find('> a').attr('aria-expanded', 'false').siblings('ul').animate({'left':'-320'}, 300).attr('aria-hidden', 'true');
+          $(this).attr('aria-expanded', "true").siblings('ul').animate({'left':'+80'}, 300).attr('aria-hidden', 'false').end().closest('li').addClass('expanded');
         }
 			});
 
