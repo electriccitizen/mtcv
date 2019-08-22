1decc483fbbe6deb3e4742edbee3378daf9217b6
broeker <tim@electriccitizen.com>
2019-08-22 16:43:43 -0500

    MT-00: update trusted host settings.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,14 +1,16 @@
 <?php
 
 /**
+ * @file
  * Load services definition file.
  */
+
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
  * Include the Pantheon-specific settings file.
  *
- * n.b. The settings.pantheon.php file makes some changes
+ * N.b. The settings.pantheon.php file makes some changes
  *      that affect all environments that this site
  *      exists in.  Always include this file, even in
  *      a local development environment, to ensure that
@@ -19,36 +21,35 @@ include __DIR__ . "/settings.pantheon.php";
 /**
  * Place the config directory outside of the Drupal root.
  */
-$config_directories = array(
+$config_directories = [
   CONFIG_SYNC_DIRECTORY => dirname(DRUPAL_ROOT) . '/config/sync',
-);
+];
 
 $config['file.settings']['make_unused_managed_files_temporary'] = TRUE;
 $config['system.file']['temporary_maximum_age'] = 30;
 
 
 
-/**
- * Trusted host patterns
- */
-$settings['trusted_host_patterns'] = [
-  '^mtcv.docksal$',
-  '^dev-mtcv.pantheonsite.io$',
-];
 
+$primary_domain = $_SERVER['HTTP_HOST'];
+
+// Drupal 8 Trusted Host Settings.
+if (is_array($settings)) {
+  $settings['trusted_host_patterns'] = ['^' . preg_quote($primary_domain) . '$'];
+}
 
 /**
- * Include docksal settings if not on Pantheon env
+ * Include docksal settings if not on Pantheon env.
  */
 
 if (!isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-$docksal_settings = __DIR__ . "/settings.docksal.php";
-if (file_exists($docksal_settings)) {
-  include $docksal_settings;
-}
+  $docksal_settings = __DIR__ . "/settings.docksal.php";
+  if (file_exists($docksal_settings)) {
+    include $docksal_settings;
+  }
 }
 /**
- * If there is a local settings file, then include it
+ * If there is a local settings file, then include it.
  */
 $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
