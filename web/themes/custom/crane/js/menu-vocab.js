4cd7371565f106eb7eb51d2422e820d7eb16eea8
Adam Fuchs <adam@electriccitizen.com>
2019-08-27 14:47:31 -0500

    Admin theming

--- web/themes/custom/crane/js/menu-vocab.js
+++ web/themes/custom/crane/js/menu-vocab.js
@@ -5,9 +5,9 @@ Drupal.behaviors.menuVocabTerms = {
     $("body", context).once('menuterms').each(function(){
       $(document).ready(function(){
         $("#block-crane-local-actions").children().each(function() {           
-          $(this).html($(this).html().replace(/term/g,"link"));
+          $(this).html($(this).html().replace(/Term/g,"Link"));
         });
-        $(".taxonomy-term-form > .form-item-prefix,#edit-relations label,#edit-weight--description,#edit-help,.vocab-nav").each(function() {           
+        $(".taxonomy-term-form > .form-item-prefix,#edit-relations label,#edit-weight--description,#edit-help,.vocab-nav,.page-title").each(function() {           
           $(this).html($(this).html().replace(/term/g,"link"));
         });
       });
