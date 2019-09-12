ec9296335784d9bd9c5e86b2607d74007380d7b8
Adam Fuchs <adam@electriccitizen.com>
2019-09-12 14:33:11 -0500

    Make accordions functional in preview and supprot book

---
+++ web/themes/custom/crane_front/js/accordion.js
@@ -0,0 +1,22 @@
+(function($, Drupal) {
+
+Drupal.behaviors.accordion = {
+    attach: function (context, settings) {
+      $(".accordion-item", context).once('accordion').each(function(){
+        //open and shut accordions on click
+        $('.accordion-header a', this).click(function(e){
+          e.preventDefault();
+          var activeHeader = $(this).parent('.accordion-header');
+          if(activeHeader.closest('.accordion-item.accord-active').length){
+            $('.accord-active').removeClass('accord-active');
+            activeHeader.next().slideUp(500).attr('aria-hidden', 'true').end().find('a').attr('aria-expanded', "false");
+          }else{
+            $('.accord-active').find('.field-text').slideUp(500).attr('aria-hidden', 'true').end().removeClass('accord-active').find('.accordion-header a').attr('aria-expanded', 'false');
+            activeHeader.parent('.accordion-item').addClass('accord-active').end().next().slideDown(500).attr('aria-hidden', 'false').end().find('a').attr('aria-expanded', "true");
+          }
+        });
+      });
+    }
+}
+
+})(jQuery, Drupal);
