599d84578aaa212bccfb2c88b927c6b164e7de28
broeker <tim@electriccitizen.com>
2019-10-11 15:32:29 -0500

    MT-00: update migrations to check for empty categories.

--- web/modules/custom/migrate_resources/src/Plugin/migrate/process/Deepen.php
+++ web/modules/custom/migrate_resources/src/Plugin/migrate/process/Deepen.php
@@ -53,7 +53,9 @@ class Deepen extends ProcessPluginBase {
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
     $keyname = (is_string($this->configuration['keyname']) && $this->configuration['keyname'] != '') ? $this->configuration['keyname'] : 'value';
 
-    if (is_array($value) || $value instanceof \Traversable) {
+    if ($value == '') {
+      return '';
+    } elseif (is_array($value) || $value instanceof \Traversable) {
       $result = [];
       foreach ($value as $sub_value) {
         $result[] = [$keyname => $sub_value];
