17aa514f906953f962a9f5f44e3a612c648402ae
broeker <tim@electriccitizen.com>
2019-10-11 14:40:01 -0500

    MT-00: update aliases for new pantheon db port.

--- drush/aliases.drushrc.php
+++ drush/aliases.drushrc.php
@@ -1,15 +1,17 @@
 <?php
-  $aliases['mtcv.dev'] = array(
-    'uri' => 'dev-mtcv.pantheonsite.io',
-    'db-url' => 'mysql://pantheon:ab97b2c93730458ab1369dbb924290aa@dbserver.dev.ad79477c-5d06-4234-b6b8-582ebeee0e5c.drush.in:21267/pantheon',
-    'db-allows-remote' => TRUE,
-    'remote-host' => 'appserver.dev.ad79477c-5d06-4234-b6b8-582ebeee0e5c.drush.in',
-    'remote-user' => 'dev.ad79477c-5d06-4234-b6b8-582ebeee0e5c',
-    'ssh-options' => '-p 2222 -o "AddressFamily inet"',
-    'path-aliases' => array(
-      '%files' => 'files',
-      '%drush-script' => 'drush',
-     ),
-  );
+
+$aliases['mtcv.dev'] = array(
+  'uri' => 'dev-mtcv.pantheonsite.io',
+  'db-url' => 'mysql://pantheon:5f9c615839e9415daa5e130f4f05baeb@dbserver.dev.ad79477c-5d06-4234-b6b8-582ebeee0e5c.drush.in:16382/pantheon',
+  'db-allows-remote' => TRUE,
+  'remote-host' => 'appserver.dev.ad79477c-5d06-4234-b6b8-582ebeee0e5c.drush.in',
+  'remote-user' => 'dev.ad79477c-5d06-4234-b6b8-582ebeee0e5c',
+  'ssh-options' => '-p 2222 -o "AddressFamily inet"',
+  'path-aliases' => array(
+    '%files' => 'files',
+    '%drush-script' => 'drush',
+  ),
+);
+
 
 
