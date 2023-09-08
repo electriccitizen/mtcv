e41123bec4e65cb8fcd8056ffd8e4181dfc25391
Adam Fuchs <adam@electriccitizen.com>
2023-09-08 08:21:22 -0500

    Training fixes

--- web/modules/custom/ec_preview/js/ec_preview.js
+++
@@ -1,9 +0,0 @@
-(function($, Drupal) {
-
-  $(document).on('click','#edit-ec-preview',function(e) {
-    e.preventDefault();
-    $('.responsive-preview-icon-active').trigger('click');
-  });
-
-
-})(jQuery, Drupal, drupalSettings);
